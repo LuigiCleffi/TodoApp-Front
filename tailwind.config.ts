4502af205092006fbe691a7af690146685ef95c3
Luigi Cleffi <luigicleffi674@gmail.com>
2024-02-21 00:19:38 -0300

    feat(config): initial config with packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "blue-dark": "#1E6F9F",
+        "blue": "#4EA8DE",
+        "purple-dark": "#5E60CE",
+        "purple": "#8D8DEA",
+        "gray-100": "#F2F2F2",
+        "gray-200": "#D9D9D9",
+        "gray-700": "#0D0D0D",
+        "gray-600": "#1A1A1A",
+        "gray-500": "#262626",
+        "gray-400": "#333333",
+        "gray-300": "#808080 ",
+      }
     },
   },
   plugins: [],
