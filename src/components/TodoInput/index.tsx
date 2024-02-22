5398b1e9f8c770d0d497973cc7898974ddccaf70
Luigi Cleffi <luigicleffi674@gmail.com>
2024-02-22 10:42:37 -0300

    feat: implemented handlers for each card

--- src/components/TodoInput/index.tsx
+++ src/components/TodoInput/index.tsx
@@ -1,10 +1,11 @@
 
-function TodoInput({ handleTaskValue, ...props }: { handleTaskValue: (e: React.ChangeEvent<HTMLInputElement>) => void }) {
+function TodoInput({ handleTaskValue, value, ...props }: { handleTaskValue: (e: React.ChangeEvent<HTMLInputElement>) => void, value: string }) {
   return (
     <input
       type="text"
       className="p-3 text-sm rounded-lg bg-gray-500 transition duration-300 ease-in-out focus:ring-1 focus:ring-purple-dark w-full text-gray-200"
       placeholder="Adicione uma nova tarefa"
+      value={value}
       onChange={handleTaskValue}
       {...props} />
   );
